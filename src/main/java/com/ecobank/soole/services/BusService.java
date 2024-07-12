28d51183009b35fd45067608362b616d38590d22
RedOx98 <olaskeet@gmail.com>
2024-07-12 16:05:34 +0100

    pagination and emailservice

--- src/main/java/com/ecobank/soole/services/BusService.java
+++ src/main/java/com/ecobank/soole/services/BusService.java
@@ -3,7 +3,8 @@ package com.ecobank.soole.services;
 import java.util.Map;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ecobank.soole.models.Bus;
 import com.ecobank.soole.payload.bus.BusFetchRequestDTO;
