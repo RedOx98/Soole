3fd25fa7307f6edc25955609d70fe86eb91bcd52
0xMarvell <rokemarvellous@gmail.com>
2024-07-25 08:14:44 +0100

    update seed data

--- src/main/java/com/ecobank/soole/config/SeedData.java
+++ src/main/java/com/ecobank/soole/config/SeedData.java
@@ -40,6 +40,12 @@ public class SeedData implements CommandLineRunner {
         Account account04 = new Account();
         Account account05 = new Account();
         Account account06 = new Account();
+        Account account07 = new Account();
+        Account account08 = new Account();
+        Account account09 = new Account();
+        Account account10 = new Account();
+        Account account11 = new Account();
+        Account account12 = new Account();
 
         // Bus bus01 = new Bus();
         // Bus bus02 = new Bus();
@@ -133,7 +139,7 @@ public class SeedData implements CommandLineRunner {
         accountService.save(account02);
         // accountService.mapToBus(account02, bus02.getBusId());
 
-        account03.setEmail("tvincent@ecobank.com");
+        account03.setEmail("captain@ecobank.com");
         account03.setPassword_hash("password");
         account03.setLevel("Executive Trainee");
         account03.setTelephone("09036123576");
@@ -184,7 +190,7 @@ public class SeedData implements CommandLineRunner {
         accountService.save(account05);
         // accountService.mapToBus(account05, bus05.getBusId());
 
-        account06.setEmail("olaomoriwo@gmail.com");
+        account06.setEmail("olaomoriwo@ecobank.com");
         account06.setPassword_hash("password");
         account06.setLevel("SBO");
         account06.setTelephone("09036123576");
@@ -193,13 +199,103 @@ public class SeedData implements CommandLineRunner {
         account06.setUsername("oomoriwo");
         account06.setAuthorities(Authority.ADMIN.toString());
         account06.setRoute("BERGER");
-        account06.setDepartment("CORPORATE BANKING");
+        account06.setDepartment("Corporate Banking");
         account06.setAffiliate("ENG");
         account06.setStaff_id("12328");
         account06.setStatus("activated");
         account06.setVerified("accepted");
         accountService.save(account06);
         // accountService.mapToBus(account05, bus05.getBusId());
+
+        account07.setEmail("user1@ecobank.com");
+        account07.setPassword_hash("password");
+        account07.setLevel("Executive Assistant");
+        account07.setTelephone("09036123576");
+        account07.setFirstName("john");
+        account07.setLastName("doe");
+        account07.setUsername("johndoe");
+        account07.setRoute("BERGER");
+        account07.setDepartment("Technology");
+        account07.setAffiliate("ENG");
+        account07.setStaff_id("10929");
+        account07.setStatus("activated");
+        account07.setVerified("APPROVED");
+        accountService.save(account07);
+
+        account08.setEmail("user2@ecobank.com");
+        account08.setPassword_hash("password");
+        account08.setLevel("Executive Assistant");
+        account08.setTelephone("09036123576");
+        account08.setFirstName("james");
+        account08.setLastName("daniel");
+        account08.setUsername("danieljames");
+        account08.setRoute("AJAH-LAKOWE");
+        account08.setDepartment("Technology");
+        account08.setAffiliate("ENG");
+        account08.setStaff_id("10930");
+        account08.setStatus("activated");
+        account08.setVerified("APPROVED");
+        accountService.save(account08);
+
+        account09.setEmail("user3@ecobank.com");
+        account09.setPassword_hash("password");
+        account09.setLevel("Executive Assistant");
+        account09.setTelephone("09036123576");
+        account09.setFirstName("graham");
+        account09.setLastName("aubrey");
+        account09.setUsername("aubreygraham");
+        account09.setRoute("IKORODU");
+        account09.setDepartment("Technology");
+        account09.setAffiliate("ENG");
+        account09.setStaff_id("10931");
+        account09.setStatus("activated");
+        account09.setVerified("APPROVED");
+        accountService.save(account09);
+
+        account10.setEmail("user4@ecobank.com");
+        account10.setPassword_hash("password");
+        account10.setLevel("Executive Assistant");
+        account10.setTelephone("09036123576");
+        account10.setFirstName("rose");
+        account10.setLastName("silva");
+        account10.setUsername("rosesilva");
+        account10.setRoute("OSHODI");
+        account10.setDepartment("Technology");
+        account10.setAffiliate("ENG");
+        account10.setStaff_id("10932");
+        account10.setStatus("activated");
+        account10.setVerified("APPROVED");
+        accountService.save(account10);
+
+        account11.setEmail("user5@ecobank.com");
+        account11.setPassword_hash("password");
+        account11.setLevel("Executive Assistant");
+        account11.setTelephone("09036123576");
+        account11.setFirstName("esther");
+        account11.setLastName("james");
+        account11.setUsername("estherjames");
+        account11.setRoute("IYANA-IPAJA");
+        account11.setDepartment("Technology");
+        account11.setAffiliate("ENG");
+        account11.setStaff_id("10933");
+        account11.setStatus("activated");
+        account11.setVerified("APPROVED");
+        accountService.save(account11);
+
+        account12.setEmail("user6@ecobank.com");
+        account12.setPassword_hash("password");
+        account12.setLevel("Executive Assistant");
+        account12.setTelephone("09036123576");
+        account12.setFirstName("benjamin");
+        account12.setLastName("button");
+        account12.setUsername("benjaminbutton");
+        account12.setRoute("AJAH-LAKOWE");
+        account12.setDepartment("Technology");
+        account12.setAffiliate("ENG");
+        account12.setStaff_id("10934");
+        account12.setStatus("activated");
+        account12.setVerified("APPROVED");
+        accountService.save(account12);
 
     }
 }
